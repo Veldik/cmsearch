331d4e97ba864a731fdc3e53a0793eb1b65778de
Velda <gamervelda@gmail.com>
2019-10-19 00:05:46 +0200

    Objektově získávám lidi

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo "cenim";
+// Přidání get.php
+require_once('get.php');
+// Proměná z URL
+$searchednick = $_GET["nick"];
+// Přidání classy
+$player = new Player($searchednick);
+// Zavolání funkce
+$player->getPlayer();
 ?>
