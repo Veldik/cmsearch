83c7c93fd415e46b7db0648e6742befd01e7d662
Jakub Velička <37436112+Veldik@users.noreply.github.com>
2019-10-22 23:21:08 +0200

    Přidání google analytics

---
+++ analytics.php
@@ -0,0 +1,13 @@
+<?php
+echo "
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src='https://www.googletagmanager.com/gtag/js?id=UA-62697528-3'></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-62697528-3');
+</script>
+";
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include("./analytics.php");
+$text = "Titulní strana bude již brzy. Pro zobrazení profilu napiš https://cm.velda.xyz/player/NICK";
+echo $text;
+?>
