9990bafac2322aff6d257cb337ba12ce12a77c8a
lilmayu <52459400+lildevuwu@users.noreply.github.com>
2019-11-30 21:00:29 +0100

    Search by pressing ENTER
    ig

--- website/header.php
+++ website/header.php
@@ -4,6 +4,7 @@ echo '
     <div class="header">
         <input type="text" placeholder="Najít hráče..." id="nick_search">
         <input type="button" value="🔍" onClick="find_player()"/>
+        // idk php, přidat možnost na zjištění eventu kdy zmáčkne uživatel ENTER aby to vyvoalalo find_player() také
     </div>
 </header>
 ';
