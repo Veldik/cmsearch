c9990b41cca88e4c811d781b1a7ad9149253eae5
Patrik Mintěl <56652391+patrick11514@users.noreply.github.com>
2020-02-27 19:41:47 +0100

    Opraven redirect na link
    Upraven link, tak aby přesměrovával na aktuální doménu.

--- website/js/main.js
+++ website/js/main.js
@@ -1,7 +1,27 @@
 function find_player() {
     var searchednick = document.getElementById("nick_search");
     var nick = searchednick.value;
-    // Jsem línej kokot a nebude to odkazovat na umístění lokální, ale prostě na můj server, kde to poběží, budu tě velice cenit, když to fixneš!
-    var link = "https://cm.velda.xyz/player/" + nick;
+    
+    var url = window.location.href.toString().split("?")[0];
+
+    if (url[url.length - 1] == "/") {
+        url = url.slice(0, -1);
+    }
+    var array = url.split("/");
+    var count = 0;
+    for(var i = 0; i < array.length; ++i){
+        if(typeof array[i] !== "undefined")
+            count++;
+    }
+    if (array[(count - 2)] == "player" ) {
+        array.splice((count - 1));
+    } else if (array[(count - 1)] == "player") {
+        array.splice((count - 1));
+        array[count - 1] = "player";
+    } else {
+        array[count] = "player";
+    }
+    var link = array.join("/") + "/" + nick;
+
     window.location = link;
 }
