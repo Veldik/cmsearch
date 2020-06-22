e47ba5f4e3a9a7296518008798edd6a0a3c819c0
Veldik <37436112+Veldik@users.noreply.github.com>
2020-06-23 01:55:29 +0200

    Oprava vyhledávání pomocí enteru

--- website/header.php
+++ website/header.php
@@ -2,7 +2,7 @@
 echo '
 <header>
     <div class="header">
-        <input type="text" placeholder="Najít hráče..." id="nick_search">
+        <input type="text" placeholder="Najít hráče..." id="nick_search" onkeypress="if(event.key == \'Enter\') {find_player()}">
         <input type="button" value="🔍" onClick="find_player()"/>
     </div>
 </header>
