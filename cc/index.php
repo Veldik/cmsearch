cdd325cf22e5b1e34164ee33f0b615b2953e7e9f
Veldik <gamervelda@gmail.com>
2020-06-23 23:25:19 +0200

    Odstranění zbytečných blbostí a přesun z /website do /

---
+++ cc/index.php
@@ -0,0 +1,7 @@
+pridan program do computer craftu, nainstalujes pomocí<br>
+wget https://cm.velda.xyz/cc/cmsearch.lua<br>
+spustis pomoci<br>
+cmsearch<br>
+funguje na 4x2 monitoru, kdyz delsi nick doporucuji 5x2 monitor<br>
+<img src="./images/example.png" height="90%"><br>
+btw tahle stranka by chtela asi nejakej design :herold:
