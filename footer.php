f34beaf63118abec5cdb85e58e8f11be71ddeca4
Veldik <gamervelda@gmail.com>
2020-06-24 14:54:24 +0200

    Přidán footer

---
+++ footer.php
@@ -0,0 +1,8 @@
+<?php
+echo '
+<footer id="sticky-footer" class="fixed-bottom py-4 bg-dark text-white">
+    <div class="container">
+        <small>Copyright &copy; 2019 - ' . date("Y") . ' <a href="https://velda.xyz">Velda</a></small>
+    </div>
+</footer>
+';
--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         CMSearch | Web bude <b>již brzy</b>.
     </div>
 </div>
-
+<?php include("./footer.php"); ?>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
         crossorigin="anonymous"></script>
