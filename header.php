fbb5dae054a221ba2583fb3a304db0573abc4d56
Veldik <gamervelda@gmail.com>
2020-06-24 14:20:23 +0200

    Změna z malého loga na velké

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 echo '
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="/">
-    <img src="/images/logo/small.png" width="40" height="40" alt="" loading="lazy">
+    <img src="/images/logo/big.png" height="40" alt="" loading="lazy">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
